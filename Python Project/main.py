35c6771c047e5c4ead653885d370e762838f4ba7
teodorradu <raduteodorgeorge0@gmail.com>
2021-02-15 15:36:16 +0100

    Implemented simple api

--- Python Project/main.py
+++ Python Project/main.py
@@ -1,3 +1,16 @@
-def get_name():
-    name = input("Please enter name: ")
-    return name
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('home.html')
+
+
+@app.route('/unquestions')
+def unquestions():
+   return render_template('unquestions.html') 
+
+
+
+if __name__ == '__main__':
+   app.run()
